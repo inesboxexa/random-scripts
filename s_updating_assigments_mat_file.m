bf814083ecc55b5f91a82ca31b654bfdc715e522
Teresa Murta <teresa.murta@npl.co.uk>
2020-06-18 11:38:19 +0100

    new threshold option
    3 times the median of the non zero intensities

--- s_updating_assigments_mat_file.m
+++ s_updating_assigments_mat_file.m
@@ -1,7 +1,8 @@
 
-threshold = 100; % threshold for the mean intensity
+dpo_path = 'X:\Beatson\Intracolonic tumour study\Data Processing Outputs DESI\';
 
-peak_assigments_folder = 'X:\PDAC Combo\az neg desi\dpo\peak assignments\';
+spectra_details_folder = [ dpo_path 'spectra details\'];
+peak_assigments_folder = [ dpo_path 'peak assignments\'];
 
 filefolder_struct = dir(peak_assigments_folder);
 
@@ -9,16 +10,41 @@ for i = 3:size(filefolder_struct)
     
     filefolder_name = filefolder_struct(i).name;
     
+    % Total Spectrum Intensities
+    
+    path = [spectra_details_folder filesep filefolder_name '\tissue only\']; % location of the total spectrum mat file
+    file = 'totalSpectrum_intensities'; % name of the total spectrum mat file
+    
+    load([ path filesep file ])
+    
+    % threshold = 100; % threshold for the mean intensity
+    
+    % 3 times the median of the total intensities threshold
+    
+    threshold = 3*median(totalSpectrum_intensities(totalSpectrum_intensities>0)); % 3 times the median of the total intensities above 0
+    
+    % Assigments
+    
     path = [peak_assigments_folder filesep filefolder_name '\tissue only\']; % location of the assigments mat file
     file = 'relevant_lists_sample_info'; % name of the assigments mat file
     
     load([ path filesep file ])
     
-    % save([path filesep 'original_' file],'relevant_lists_sample_info','-v7.3') % save original assignments mat file
+    figure('Name',filefolder_name); 
+    plot(totalSpectrum_mzvalues, totalSpectrum_intensities, 'k');
+    hold on; stem(totalSpectrum_mzvalues(totalSpectrum_intensities>threshold),totalSpectrum_intensities(totalSpectrum_intensities >= threshold),'.r')
     
-    mean_intensity = double(relevant_lists_sample_info(:,11)); % collect mean intensities array
+    save([path filesep 'original_' file],'relevant_lists_sample_info','-v7.3') % save original assignments mat file
     
-    relevant_lists_sample_info(mean_intensity<threshold,:) = []; % remove mean intensities below threshold
+    % Mean intensity based threshold
+    
+    % mean_intensity = double(relevant_lists_sample_info(:,11)); % mean intensities array
+    % relevant_lists_sample_info(mean_intensity<threshold,:) = []; % remove mean intensities below threshold
+    
+    % 3x the median of the total intensities threshold
+    
+    total_intensity = double(relevant_lists_sample_info(:,6)); % total intensities array
+    relevant_lists_sample_info(total_intensity < threshold,:) = []; % remove mean intensities below threshold
     
     save([path filesep file],'relevant_lists_sample_info','-v7.3') % save new assignments mat file
     
